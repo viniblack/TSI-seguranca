b291e0a778e6f508f6d47af9608c1d51aceb02d1
ViniBlack <viniibl4ck@gmail.com>
2023-05-17 18:14:40 -0300

    sera que foi?

--- dump/dump.sql
+++ dump/dump.sql
@@ -6,44 +6,47 @@
 -- Tempo de geração: 10-Maio-2023 às 01:27
 -- Versão do servidor: 10.4.22-MariaDB
 -- versão do PHP: 8.1.2
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */
+;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */
+;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */
+;
+/*!40101 SET NAMES utf8mb4 */
+;
 --
 -- Banco de dados: `security`
 --
 
 -- --------------------------------------------------------
-
 --
 -- Estrutura da tabela `tarefas`
 --
 
+CREATE DATABASE IF NOT EXISTS `security` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
 CREATE TABLE `tarefas` (
   `id` int(11) NOT NULL,
   `descricao` varchar(255) NOT NULL,
   `concluida` tinyint(1) NOT NULL DEFAULT 0,
   `data_criacao` timestamp NOT NULL DEFAULT current_timestamp(),
   `id_usuario` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
 --
 -- Extraindo dados da tabela `tarefas`
 --
 
-INSERT INTO `tarefas` (`id`, `descricao`, `concluida`, `data_criacao`, `id_usuario`) VALUES
-(8, 'estudar', 1, '2023-05-09 22:19:04', 2);
-
+INSERT INTO `tarefas` (
+    `id`,
+    `descricao`,
+    `concluida`,
+    `data_criacao`,
+    `id_usuario`
+  )
+VALUES (8, 'estudar', 1, '2023-05-09 22:19:04', 2);
 -- --------------------------------------------------------
-
 --
 -- Estrutura da tabela `usuarios`
 --
@@ -52,16 +55,22 @@ CREATE TABLE `usuarios` (
   `id` int(11) NOT NULL,
   `username` varchar(50) NOT NULL,
   `password` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
 --
 -- Extraindo dados da tabela `usuarios`
 --
 
-INSERT INTO `usuarios` (`id`, `username`, `password`) VALUES
-(1, 'rerossi', '$2y$10$DEuKelYXMHtc6kPOlE8kQuk0aID6EYpCj8PNo/dBJiSGbd2jyLrHe'),
-(2, 'rodrigo', '$2y$10$pKDSVM7tc4BN8ujiIBwmHuk2JAUBZhLbP3wYgd4UrU8quHCEveoy6');
-
+INSERT INTO `usuarios` (`id`, `username`, `password`)
+VALUES (
+    1,
+    'vini',
+    '$2y$10$DEuKelYXMHtc6kPOlE8kQuk0aID6EYpCj8PNo/dBJiSGbd2jyLrHe'
+  ),
+  (
+    2,
+    'rodrigo',
+    '$2y$10$pKDSVM7tc4BN8ujiIBwmHuk2JAUBZhLbP3wYgd4UrU8quHCEveoy6'
+  );
 --
 -- Índices para tabelas despejadas
 --
@@ -70,15 +79,13 @@ INSERT INTO `usuarios` (`id`, `username`, `password`) VALUES
 -- Índices para tabela `tarefas`
 --
 ALTER TABLE `tarefas`
-  ADD PRIMARY KEY (`id`),
+ADD PRIMARY KEY (`id`),
   ADD KEY `fk_tarefas_usuarios` (`id_usuario`);
-
 --
 -- Índices para tabela `usuarios`
 --
 ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`id`);
-
+ADD PRIMARY KEY (`id`);
 --
 -- AUTO_INCREMENT de tabelas despejadas
 --
@@ -87,14 +94,14 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de tabela `tarefas`
 --
 ALTER TABLE `tarefas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
-
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,
+  AUTO_INCREMENT = 9;
 --
 -- AUTO_INCREMENT de tabela `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,
+  AUTO_INCREMENT = 3;
 --
 -- Restrições para despejos de tabelas
 --
@@ -103,9 +110,11 @@ ALTER TABLE `usuarios`
 -- Limitadores para a tabela `tarefas`
 --
 ALTER TABLE `tarefas`
-  ADD CONSTRAINT `fk_tarefas_usuarios` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id`);
+ADD CONSTRAINT `fk_tarefas_usuarios` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id`);
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */
+;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */
+;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */
+;
